8a158e53322a9ef91a13b90029ddab9e210ec367
Lihue Leandro Luna <lihue.luna@mi.unc.edu.ar>
2022-06-06 17:42:10 -0300

    asdeef

--- lcd_interface.c
+++ lcd_interface.c
@@ -7,25 +7,20 @@
 
 int main(){
     FILE * file = fopen("/dev/display_kernel","w");
-    char buffer[512];
-    buffer[0]='\0';
+    char buffer[32];
     if(!file){
         printf("Error al abrir CDD");
         exit(EXIT_FAILURE); 
     }
     while(-1){
         write(1,"Ingrese string a enviar al display: ",37);
-        if(read(0,buffer,512)>32) {
-            printf("El string tiene mas de 32 caracteres\n");
-        }else{
-            buffer[strlen(buffer)-1] = '\0';
-            if(strcmp(buffer,"salir")==0){
-                fclose(file);
-                return 0;
-            }
-            fwrite(buffer,sizeof(char),strlen(buffer),file);
-            fflush(file);
-            buffer[0]='\0';
+        read(0,buffer,32);
+        buffer[strlen(buffer)-1] = '\0';
+        if(strcmp(buffer,"salir")==0){
+            fclose(file);
+            return 0;
         }
-    }
+        fwrite(buffer,sizeof(char),strlen(buffer),file);
+        fflush(file);
+    }    
 }
