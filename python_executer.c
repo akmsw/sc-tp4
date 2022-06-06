243d1bc1f9c133ae76f9560644fe2a67375a0c56
Lihue Leandro Luna <lihue.luna@mi.unc.edu.ar>
2022-06-06 15:26:07 -0300

    asdsdsd

--- python_executer.c
+++ python_executer.c
@@ -3,8 +3,8 @@
 #include <string.h>
 #include <unistd.h>
 
-int main(){
-    char*argv[] = {"/usr/bin/python3","/home/liwex/lcd/lcd_write.py","ABCD",NULL};
-    execv(argv[0],argv);
+int main(int argc,char **argv){
+    char*args[] = {"/usr/bin/python3","/home/liwex/lcd/lcd_write.py",argv[1],NULL};
+    execv(args[0],args);
     return 0;    
 }
