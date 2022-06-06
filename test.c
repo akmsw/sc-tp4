5fd744b44c97f879d2e292deba35da934781c4af
Lihue Leandro Luna <lihue.luna@mi.unc.edu.ar>
2022-06-06 14:51:29 -0300

    test fix

--- test.c
+++ test.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 int main(){
-    FILE * file = fopen("/dev/display_kernel","w");
-    fwrite("asd",1,4,file);
-    fclose(file);
+    int file = open("/dev/display_kernel","w");
+    write(file,"ABCD",4);
+    close(file);
     return 0;
 }
