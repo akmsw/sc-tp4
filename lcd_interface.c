d6f224716f2d6049eb86484a234b004e37f2474b
Lihue Leandro Luna <lihue.luna@mi.unc.edu.ar>
2022-06-06 17:55:11 -0300

    asdasdd

--- lcd_interface.c
+++ lcd_interface.c
@@ -4,18 +4,23 @@
 #include <unistd.h>
 #include <signal.h>
 
-
 int main(){
     FILE * file = fopen("/dev/display_kernel","w");
-    char buffer[32];
+    char buffer[512];
+    bzero(buffer,512);
     if(!file){
         printf("Error al abrir CDD");
         exit(EXIT_FAILURE); 
     }
+    
     write(1,"Ingrese string a enviar al display: ",37);
-    read(0,buffer,31);
-    buffer[31] = '\0';
-    fwrite(buffer,sizeof(char),strlen(buffer),file);
+    if(read(0,buffer,512)>32) {
+        printf("El string tiene mas de 32 caracteres\n");
+    }else{
+        buffer[strlen(buffer)-1] = '\0';
+        fwrite(buffer,sizeof(char),strlen(buffer),file);
+    }
     fclose(file);
-    return 0;    
+    return 0;
+
 }
