dcda57340d8912e34bfc511b34ecfd3a2780ea56
Lihue Leandro Luna <lihue.luna@mi.unc.edu.ar>
2022-06-06 17:12:44 -0300

    sdsdsd

--- lcd_interface.c
+++ lcd_interface.c
@@ -8,6 +8,7 @@
 int main(){
     FILE * file = fopen("/dev/display_kernel","w");
     char buffer[512];
+    bzero(buffer,512);
     if(!file){
         printf("Error al abrir CDD");
         exit(EXIT_FAILURE); 
@@ -17,6 +18,7 @@ int main(){
     if(read(STDIN_FILENO,buffer,512)>32) {
         printf("El string tiene mas de 32 caracteres\n");
     }else{
+        buffer[strlen(buffer)] = "\0";
         fwrite(buffer,sizeof(char),strlen(buffer),file);
     }
     fclose(file);
