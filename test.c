aad912c4a5c741f95fa6c0341367f78ce537dc74
Lihue Leandro Luna <lihue.luna@mi.unc.edu.ar>
2022-06-06 14:59:45 -0300

    dsaaa

--- test.c
+++ test.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <fcntl.h>
-#include <unistd.h>
 
 int main(){
-    int file = open("/dev/display_kernel",O_RDWR);
-    write(file,"ABCD",4);
-    close(file);
+    FILE * file = fopen("/dev/display_kernel","w");
+    if(!file){
+        printf("Exploto");
+        exit(EXIT_FAILURE); 
+    }
+    fwrite("ABCD",sizeof(char),4,file);
+    fclose(file);
     return 0;
 }
