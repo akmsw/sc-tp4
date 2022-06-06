bd987b00ca8543e13770c0991f11db24065c65fd
Lihue Leandro Luna <lihue.luna@mi.unc.edu.ar>
2022-06-06 17:51:44 -0300

    asdsadasd

--- lcd_interface.c
+++ lcd_interface.c
@@ -12,18 +12,10 @@ int main(){
         printf("Error al abrir CDD");
         exit(EXIT_FAILURE); 
     }
-    while(-1){
-        write(1,"Ingrese string a enviar al display: ",37);
-        read(0,buffer,31);
-        char * clear = "                              \0";
-        fwrite(clear,sizeof(char),32,file);
-        fflush(file);
-        buffer[31] = '\0';
-        if(strcmp(buffer,"salir")==0){
-            fclose(file);
-            return 0;
-        }
-        fwrite(buffer,sizeof(char),strlen(buffer),file);
-        fflush(file);
-    }    
+    write(1,"Ingrese string a enviar al display: ",37);
+    read(0,buffer,31);
+    buffer[31] = '\0';
+    fwrite(buffer,sizeof(char),strlen(buffer),file);
+    fclose(file);
+    return 0;    
 }
