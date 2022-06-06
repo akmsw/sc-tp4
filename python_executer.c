124b9799e520d8d09c544135599f594b99b76834
Lihue Leandro Luna <lihue.luna@mi.unc.edu.ar>
2022-06-06 16:45:01 -0300

    dede

--- python_executer.c
+++ python_executer.c
@@ -4,7 +4,7 @@
 #include <unistd.h>
 
 int main(int argc,char **argv){
-    char*args[] = {"/usr/bin/python3","/user/bin/lcd_write.py",argv[1],NULL};
+    char*args[] = {"/usr/bin/python3","/usr/bin/lcd_write.py",argv[1],NULL};
     execv(args[0],args);
     return 0;    
 }
