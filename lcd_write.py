1d9d8dd436db3f96d9f303494b431cbd851fc8bf
Lihue Leandro Luna <lihue.luna@mi.unc.edu.ar>
2022-06-06 13:02:07 -0300

    writer sepaara las lineas de a 16 caracteres

--- lcd_write.py
+++ lcd_write.py
@@ -3,4 +3,12 @@ import sys
 
 string = sys.argv[1]
 mylcd = LCD.lcd()
-mylcd.lcd_display_string(string,1)
+mylcd.lcd_clear()
+if(len(string)>16):
+    string1 = string[:16]
+    string2 = string[16:]
+    mylcd.lcd_display_string(string1,1)
+    mylcd.lcd_display_string(string2,2)
+else:
+    mylcd.lcd_display_string(string,1)
+
